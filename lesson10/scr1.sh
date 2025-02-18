fd2b7883f99ec6c16d1c408248d67ddae1844533
omichan <evgeny@dingis.ru>
2025-02-18 20:22:07 +0600

    lesson 11 changes

--- lesson10/scr1.sh
+++ lesson10/scr1.sh
@@ -1,57 +1,57 @@
 #!/bin/bash
-f1name=1
-f2name=2
-f3name=3
-f4name=4
-f5name=5
+# names for new files
+F1NAME=1
+F2NAME=2
+F3NAME=3
+F4NAME=4
+F5NAME=5
+# folder name for script
+SCRFOLDER="myfolder"
+# random length for 3d file: 100 = NUMOFRAND * 5
+NUMOFRAND=20
 
 #get current user name
-iuser=`whoami`
+iuser=$(whoami)
 
-#1 step
-fullpath=/home/$iuser/myfolder
-
-if [ ! -d $fullpath ] # Проверка наличия файла.
+# get full path for script
+fullpath=/home/$iuser/$SCRFOLDER
+# step 1: create new folder
+if [ ! -d "$fullpath" ]
 then
-	mkdir $fullpath
+	mkdir "$fullpath"
 	echo "$fullpath is created!"
 else
 	echo "Folder is exist!"
 fi
 
 
-#2 step
-for arg in "$f1name" "$f2name" "$f3name" "$f4name" "$f5name"
+#2 step: create new files
+for arg in "$F1NAME" "$F2NAME" "$F3NAME" "$F4NAME" "$F5NAME"
 do
 	touch "${fullpath}/${arg}"
 	echo "File $arg created!"
 done
 
-#2.1 step
-curdate=`date`
-echo -e "Hello !! \n${curdate}" > "${fullpath}/${f1name}"
-#echo "Hello !! \n ${curdate}" > "${fullpath}/${f1name}"
-#2.2 step
-echo "" > "${fullpath}/${f2name}"
-exec `chmod ugo+rwx "${fullpath}/${f2name}"`
-#2.3 step
+#2.1 step: write greeting and date to file
+curdate=$(date)
+echo -e "Hello !! \n${curdate}" > "${fullpath}/${F1NAME}"
+#echo "Hello !! \n ${curdate}" > "${fullpath}/${F1NAME}"
+#2.2 step: change rights to 2d file
+echo "" > "${fullpath}/${F2NAME}"
+chmod ugo+rwx "${fullpath}/${F2NAME}"
+#2.3 step: generate random string from DICT by $RANDOM function
 randomvar=''
 randDICT="abcdefghijklmnopqrstuvwxyzABCDIFGHIJKLMNOPQRSTUVWXYZ0123456789"
-lengthDICT=${#randDICT}
-for num in {0..20}
+
+for num in $(seq 0 NUMOFRAND)
 do
 	tempnum=$RANDOM
-	let "tempnum %= 64"
+	(("tempnum %= 64"))
 	randomvar="${randomvar}${randDICT:$tempnum:1}"
-	#echo ${randDICT:$tempnum:1}
-	#echo $tempnum
-	#echo $randomvar
 done
 
-#echo $tempnum
-#echo $randomvar
-#2.3 step
-echo $randomvar > "${fullpath}/${f3name}"
-#2.4 step
-echo "" > "${fullpath}/${f4name}"
-echo "" > "${fullpath}/${f5name}"
+#write random string to 3d file
+echo "$randomvar" > "${fullpath}/${F3NAME}"
+#2.4 step: clear 4d and 5d files
+echo "" > "${fullpath}/${F4NAME}"
+echo "" > "${fullpath}/${F5NAME}"
